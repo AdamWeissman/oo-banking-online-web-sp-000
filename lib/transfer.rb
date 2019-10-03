711c4e2bac57d1517706087c3c36c7267bcc73bc
Adam Weissman <adam@comedytragedyepic.com>
2019-10-03 09:46:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,5 +12,9 @@ class Transfer
     (sender.valid? && receiver.valid?) ? true : false
   end
   
+  def execute_transaction
+    sender.balance -= amount
+    receiver.balance += amount
+  end
 end
   
