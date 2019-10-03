ed71c062b82f42a90848dfec3dbf3c1d5312aadf
Adam Weissman <adam@comedytragedyepic.com>
2019-10-03 09:47:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,13 +8,14 @@ class Transfer
     @status = "pending"
   end
   
-  def valid?
+  def self.valid?
     (sender.valid? && receiver.valid?) ? true : false
   end
   
-  def execute_transaction
-    sender.balance -= amount
-    receiver.balance += amount
-  end
+  #def execute_transaction
+    #if self.valid
+    #sender.balance -= amount
+    #receiver.balance += amount
+  #end
 end
   
