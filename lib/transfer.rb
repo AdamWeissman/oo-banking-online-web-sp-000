9524ef6ce40d900fa3141023f0506f3c0120888d
Adam Weissman <adam@comedytragedyepic.com>
2019-10-03 10:09:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
       return status
-    else
+    elsif
       return "Transaction rejected. Please check your account balance."
     end
   end
